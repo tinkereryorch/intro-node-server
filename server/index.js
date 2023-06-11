2953f15dbf4ea8a2a107cf07111857b5bb4ef57e
Jorge Garcia <jegcelorio@yahoo.com>
2023-06-11 18:20:19 -0500

    Converted text response to JSON

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type': 'text/plain',
     });
-    res.end('Hello! The Doom guy is your friend!');
+    res.end(JSON.stringify({
+        id: 1,
+        name: 'Doom Guy',
+    }));
 });
 
 server.listen(PORT, () => {
