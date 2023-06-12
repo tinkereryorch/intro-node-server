d5920b5f07badb15688836f2d5951b4b4c19e15d
Jorge Garcia <jegcelorio@yahoo.com>
2023-06-11 20:05:16 -0500

    Refactored basic response code

--- server/index.js
+++ server/index.js
@@ -2,13 +2,15 @@ const http = require('http');
 
 const PORT = 3002;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain',
-    });
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+
     res.end(JSON.stringify({
         id: 1,
-        name: 'Doom Guy',
+        name: 'Doom Guy'
     }));
 });
 
