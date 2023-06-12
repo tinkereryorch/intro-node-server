a8b0977dc335fc0bc09c7c5d809751a15b9b77fb
Jorge Garcia <jegcelorio@yahoo.com>
2023-06-11 20:42:50 -0500

    Added friends array handler

--- server/index.js
+++ server/index.js
@@ -21,14 +21,16 @@ const friends = [
 ];
 
 server.on('request', (req, res) => {
-    if (req.url === '/friends') {
+    const items = req.url.split('/');
+    if (items[1] === 'friends') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-
-        res.end(JSON.stringify({
-            id: 1,
-            name: 'Doom Guy'
-        }));
+        if (items.length === 3) {
+            const friendIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendIndex]));
+        } else {
+            res.end(JSON.stringify(friends));
+        }
     } else if (req.url === '/messages') {
         res.write('<html>');
         res.write('<body>');
