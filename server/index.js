b8034929fcc0d55e3d0a7e9b6ad0afd11402d98d
Jorge Garcia <jegcelorio@yahoo.com>
2023-06-11 20:07:40 -0500

    Added specific routes

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const { resolveNaptr } = require('dns');
 const http = require('http');
 
 const PORT = 3002;
@@ -5,13 +6,18 @@ const PORT = 3002;
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-
-    res.end(JSON.stringify({
-        id: 1,
-        name: 'Doom Guy'
-    }));
+    if(req.url === '/friends'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+    
+        res.end(JSON.stringify({
+            id: 1,
+            name: 'Doom Guy'
+        }));
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
 });
 
 server.listen(PORT, () => {
